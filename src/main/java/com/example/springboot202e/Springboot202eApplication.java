29aaf04a93480d915bdff922c48351a31b78ed50
Yifty <yifty08@icloud.com>
2019-05-14 09:50:24 -0400

    creating java beans

---
+++ src/main/java/com/example/springboot202e/Springboot202eApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot202e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot202eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot202eApplication.class, args);
+	}
+
+}
